16f652d59a7080ee9725b6abddfc9f3bd6ef4c72
adambourhim <benbourhim7@gmail.com>
2022-03-12 14:30:16 +0100

    first_comit

---
+++ src/component/contact/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contacts : Adam Ben Bourhim</h1>
+        <ul>
+          <li>Tel : 0632727991 </li>
+          <li>adamborhim@gmail.com</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Contact;
