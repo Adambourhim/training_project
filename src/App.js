bfe1be6b7bb57b0c20e19112f9039a565d4d7e71
adambourhim <benbourhim7@gmail.com>
2022-03-24 14:19:40 +0100

    event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Contacts from "./component/contact/Contacts";
 import Navbar from "./component/navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
 function App() {
   return (
--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./contact.css";
 class Contact extends Component {
+  showContact(message) {
+    console.log("salam", message);
+  }
   render() {
     const { name, tel, email } = this.props.data;
     return (
       <div className="card">
         <div className="card-body">
-          <h4 className="card-title">{name}</h4>
+          <h4 className="card-title">
+            {name}
+            <i
+              onClick={this.showContact.bind(this, name)}
+              className="fa fa-sort-down"
+            ></i>
+          </h4>
           <p className="card-text">
             <ul className="list-group">
               <li className="list-group-item">{tel}</li>
