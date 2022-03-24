1bafa9e21bf2a4db54ee4bb38d7e2093c363314d
adambourhim <benbourhim7@gmail.com>
2022-03-24 14:19:32 +0100

    event_react

--- src/component/contact/Contacts.js
+++ src/component/contact/Contacts.js
@@ -12,6 +12,12 @@ class Contacts extends Component {
       { id: 2, name: "salma", tel: "0634862162", email: "salma@gmail.com" },
       { id: 3, name: "hamza", tel: "0634862163", email: "hamza@gmail.com" },
       { id: 4, name: "inssaf", tel: "0634862164", email: "inssaf@gmail.com" },
+      {
+        id: 5,
+        name: "mohammed",
+        tel: "0634862165",
+        email: "mohamed@gmail.com",
+      },
     ],
   };
 
